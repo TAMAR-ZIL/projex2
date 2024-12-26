84b625869ab783883b276abdd750ea720d162f84
tamar <t0527199526@gmail.com>
2024-12-26 17:44:06 +0200

    my second commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete'
 import { useState } from "react"
 import { MapContainer, TileLayer, useMap,Popup ,Marker} from 'react-leaflet'
+function SetViewOnChange({ coords }) {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(coords, map.getZoom());
+  }, [coords, map]);
+
+  return null;
+}
 export default function Form() {
   let [data, setData] = useState([]);
   let [val, setVal] = useState("")
   let [lat, setLat] = useState(51.505);
   let [lon, setLone] = useState(-0.09)
-  const findAddress = async (value) => {
+    const findAddress = async (value) => {
 
   
     if (!value) {
@@ -25,7 +34,7 @@ export default function Form() {
     catch (error) { console.log(error) }
   }
   return (
-    <>
+    <div className='form-container'>
     <Autocomplete
       disablePortal
       options={data}
@@ -43,18 +52,21 @@ export default function Form() {
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="הכנס כתובת" />}
     />
+    <div className='map-wrapper'>
     <MapContainer className='map'  center={[lat, lon]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
+  <SetViewOnChange coords={[lat, lon]} />
   <Marker position={[lat,lon]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
   </Marker>
 </MapContainer>
-    </>
+</div>
+    </div>
   );
 }
 
